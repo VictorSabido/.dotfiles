42c38369921ad7e60e80f294714dedd0e2e518cc
VictorSabido <vsabidorubio@gmail.com>
2021-07-16 11:56:08 +0200

    Check changes and update

---
+++ check-changes.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ERROR='\033[0;31m'
+NO_FORMAT="\033[0m"
+GREEN="\e[32m"
+C_DODGERBLUE1="\033[38;5;33m"
+
+git fetch
+HEADHASH=$(git rev-parse HEAD)
+UPSTREAMHASH=$(git rev-parse master@{upstream})
+
+ColorGreen(){
+    echo $GREEN$1$NO_FORMAT
+}
+
+gitpull(){
+    echo "Aplicando cambios de remoto ..."
+    git pull origin
+    echo "$(ColorGreen 'Cambios aplicados!')"
+}
+
+if [ "$HEADHASH" != "$UPSTREAMHASH" ]
+then
+    echo "${ERROR}.dotfiles tiene cambios en remoto que no están en local ${NO_FORMAT}"
+    echo
+    echo "${C_DODGERBLUE1}¿Quieres actualizar .dotfiles?${NO_FORMAT}"
+    echo "
+        $(ColorGreen '1)') Si
+        $(ColorGreen '2)') No
+    "
+    read a
+    case $a in
+        1) gitpull ;;
+        2) exit 0 ;;
+        *) exit 0 ;;
+    esac
+fi
--- dots_install.sh
+++ dots_install.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 ln -fs .dotfiles/.bash_aliases $HOME/.bash_aliases
 ln -fs .dotfiles/.zshrc $HOME/.zshrc
 ln -fs .dotfiles/.p10k.zsh $HOME/.p10k.zsh
 ln -fs .dotfiles/.gitconfig $HOME/.gitconfig
 
-source ~/.zshrc
+sh ~/.zshrc
